763a55fcbe2cb1ad8f469fbbf0f1d6782dd68bd8
Charanya Gade <charanyagade@gmail.com>
2025-11-08 08:18:00 +0530

    Updated v1.0

--- frontendreactapp/frontendreactapp/src/config.js
+++ frontendreactapp/frontendreactapp/src/config.js
@@ -1,6 +1,6 @@
 const config = 
 {
-    "url":"http:ec2-3-144-205-208.us-east-2.compute.amazonaws.com:2066"
+    "url":"ec2-18-117-130-219.us-east-2.compute.amazonaws.com:2066"
 }
 
 export default config
