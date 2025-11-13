6f3e3c3aab9a7eb02c6d1bc8f9f60ac2f40c5b97
CharanyaGade <163649457+CharanyaGade@users.noreply.github.com>
2025-11-13 10:20:09 +0530

    Update API URL in config.js

--- frontendreactapp/frontendreactapp/src/config.js
+++ frontendreactapp/frontendreactapp/src/config.js
@@ -1,6 +1,6 @@
 const config = 
 {
-    "url":"ec2-18-117-131-128.us-east-2.compute.amazonaws.com:2066/"
+    "url":"ec2-18-222-173-113.us-east-2.compute.amazonaws.com:2066/"
 }
 
 export default config
