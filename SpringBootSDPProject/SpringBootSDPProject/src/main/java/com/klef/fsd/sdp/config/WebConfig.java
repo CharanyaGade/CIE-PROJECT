ecf6e654d70402ff241cb4e4f6ec56324bcfbadb
Charanya Gade <charanyagade@gmail.com>
2025-11-13 10:51:09 +0530

    added config

---
+++ SpringBootSDPProject/SpringBootSDPProject/src/main/java/com/klef/fsd/sdp/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.klef.fsd.sdp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "http://ec2-18-222-173-113.us-east-2.compute.amazonaws.com",
+                            "http://ec2-18-222-173-113.us-east-2.compute.amazonaws.com:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
